00707c53d2f621d51d4fd32738f6190ea1830bb7
kushall1845 <kushall1845@gmail.com>
2025-10-07 18:43:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, Kushal_Devops");
-     System.out.println("Hello, Jenkins01_Kushal_Kumar");
+     System.out.println("Hello, Jenkins03_Kushal_Kumar");
      System.out.println("Hello, Jenkins_teams-using-webhooks"); 
 
   }
